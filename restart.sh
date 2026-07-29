26a9707cb8ee2f370003093fbe2e574a0acbf2da
Magnus Manske <magnusmanske@proton.me>
2026-07-29 12:37:20 +0000

    misc

--- restart.sh
+++ restart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 toolforge webservice stop
-toolforge webservice buildservice start --mount=none --health-check-path '/healthz'
+toolforge webservice buildservice start --mount=all --health-check-path '/healthz'
