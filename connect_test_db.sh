5eeddb768ae17f7a1ec2d140621c5ff764f4f06f
Magnus Manske <magnusmanske@googlemail.com>
2026-07-29 12:07:59 +0300

    Create connect_test_db.sh

---
+++ connect_test_db.sh
@@ -0,0 +1,2 @@
+ssh magnus@login.toolforge.org -L 3309:wikidatawiki.web.db.svc.eqiad.wmflabs:3306 -N &
+ssh magnus@login.toolforge.org -L 3317:termstore.wikidatawiki.analytics.db.svc.wikimedia.cloud:3306 -N &
